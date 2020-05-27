953b779c5ad26248ea809affac8f1eb5855f14d0
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-27 19:17:44 +0800

    Created gratitude.js sequelize model

---
+++ models/gratitude.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+  const Gratitude = sequelize.define("Gratitude", {
+    gratitudeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    gratitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    action: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    public: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+  return Gratitude;
+};
