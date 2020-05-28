b4abf7ad4f674912695211b70f66bdd95e7e301f
Caroline <caroline.portilla@gmail.com>
2020-05-28 14:08:16 +0800

    logout button functionality

--- public/assets/js/gratitude.js
+++ public/assets/js/gratitude.js
@@ -1,20 +1,26 @@
+/* eslint-disable no-undef */
 $(document).ready(function() {
 //Adding modal functionality when login and signup buttons are clicked
 $("#login-button").click(function() {
     $(".login").addClass("is-active");  
   });
   
-  $(".modal-close").click(function() {
-     $(".modal").removeClass("is-active");
-  });
+$(".modal-close").click(function() {
+   $(".modal").removeClass("is-active");
+});
 
-  $(".cancel-button").click(function() {
-    $(".modal").removeClass("is-active");
+$(".cancel-button").click(function() {
+   $(".modal").removeClass("is-active");
+});
+
+$("#signup-button").click(function() {
+   $(".signup").addClass("is-active");  
+});
+
+//Return to main screen when logout button is clicked.
+$("#logout-button").click(function() {
+   window.location.replace("/");
  });
-
- $("#signup-button").click(function() {
-    $(".signup").addClass("is-active");  
-  });
 
   //Sets the date
   const updateTime = function() {
