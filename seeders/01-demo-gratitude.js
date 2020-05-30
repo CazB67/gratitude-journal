23029f59b95a0ea6c285a2e86239e26ab2e07771
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-05-30 13:02:46 +0800

    Added seeder for gratitude table

---
+++ seeders/01-demo-gratitude.js
@@ -0,0 +1,31 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.bulkInsert('Gratitude', [{
+        description: 'Saw nana today and we had a great chat.',
+        action: 'Helped someone carry their groceries.',
+        shareable: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 1
+      },
+      {
+        description: 'Had a coffee with my friends',
+        action: 'Donated to the local food bank.',
+        shareable: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 2
+      },
+      {
+        description: 'Went for a long walk along the beach',
+        action: 'Paid for someone elses meal the the restaurant.',
+        shareable: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: 3
+      }]);
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('Gratitude', null, {});
+    }
+  };
