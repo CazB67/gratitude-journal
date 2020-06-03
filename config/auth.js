b77e08bae213d1eb04f187a30182b13747c127f7
Caroline <caroline.portilla@gmail.com>
2020-06-03 21:19:25 +0800

    facebook url

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
     'facebookAuth' : {
-        'callbackURL': 'https://safe-dawn-22162.herokuapp.com//auth/facebook/callback'
+        'callbackURL': 'https://safe-dawn-22162.herokuapp.com/auth/facebook/callback'
     }
 }
