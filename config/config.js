b4713b4228634cd12e05fbd97205494421a11d1b
Caroline <caroline.portilla@gmail.com>
2020-09-07 21:25:08 +0800

    testing git

--- config/config.js
+++ config/config.js
@@ -2,16 +2,16 @@
 
 module.exports = 
 {
-  "development": {
+  "development": { 
     "username": "root",
     "password": process.env.PASSWORD,
     "database": "gratitude_journal_db",
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "OscarChloeLeo1!",
     "database": "database_test", 
     "host": "127.0.0.1",
     "dialect": "mysql"
