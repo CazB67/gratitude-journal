00879212e86f27ff0814a1dc9482e00ed31e6e1f
Caroline Bates <caroline.portilla@gmail.com>
2024-05-07 07:43:18 +0800

    sql

--- config/config.js
+++ config/config.js
@@ -17,7 +17,13 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable":"JAWSDB_URL",
-    "dialect": "mysql"
+    "POSTGRES_URL":"postgres://default:bZPrMkvVx87Q@ep-nameless-flower-a71fi9vc-pooler.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require",
+    "POSTGRES_PRISMA_URL":"postgres://default:bZPrMkvVx87Q@ep-nameless-flower-a71fi9vc-pooler.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
+    "POSTGRES_URL_NO_SSL":"postgres://default:bZPrMkvVx87Q@ep-nameless-flower-a71fi9vc-pooler.ap-southeast-2.aws.neon.tech:5432/verceldb",
+    "POSTGRES_URL_NON_POOLING":"postgres://default:bZPrMkvVx87Q@ep-nameless-flower-a71fi9vc.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require",
+    "POSTGRES_USER":"default",
+    "POSTGRES_HOST":"ep-nameless-flower-a71fi9vc-pooler.ap-southeast-2.aws.neon.tech",
+    "POSTGRES_PASSWORD":"bZPrMkvVx87Q",
+    "POSTGRES_DATABASE":"verceldb"
   }
 }
